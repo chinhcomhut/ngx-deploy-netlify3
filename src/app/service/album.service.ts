234781fa6ad9262993c34554bf5e27cd1b658f21
chinhcomhut <chinhcomhut@gmail.com>
2020-09-08 01:00:22 +0700

    up

--- src/app/service/album.service.ts
+++ src/app/service/album.service.ts
@@ -10,11 +10,12 @@ import {environment} from '../../environments/environment';
 export class AlbumService {
   //API LOCAL
   // private API_Album = environment.URL_local+'album';
-  private API_List_Album = environment.URL_local+'list-album';
+  // private API_List_Album = environment.URL_local+'list-album';
 
   //API SEVER
   private API_Album = environment.URL_server+'album';
   private API_Play_List_By_Album = environment.URL_server+'playlist-by-album';
+  private API_List_Album = environment.URL_server+'list-album';
   constructor(private http: HttpClient) { }
   createAlbum(album: AlbumInfo): Observable<AlbumInfo>{
     return this.http.post<AlbumInfo>(this.API_Album, album);
